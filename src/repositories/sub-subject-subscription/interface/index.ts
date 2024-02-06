f079e415f2fa8706aaccb71250f1cafa7434465c
Karl-Johan Bailey <karl@cenedex.com>
2024-02-06 19:33:23 -0400

    feat: repository changes with subject

--- src/repositories/sub-subject-subscription/interface/index.ts
+++ src/repositories/sub-subject-subscription/interface/index.ts
@@ -6,5 +6,9 @@ export type SubSubjectSubscriptionRepositoryType = RepositoryDatabaseBaseType & 
     student_id: string; 
     sub_subject_id: string;
     status: 'active' | 'paused' | 'cancelled';
+    paid_for_by: 'free' | 'paid';
+    payment_method: 'stripe' | 'wipay';
+    metadata: Record<string, string>;
+    cancelled_at?: Date;
     expires_at: Date;
 }
